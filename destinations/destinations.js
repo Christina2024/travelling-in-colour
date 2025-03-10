c56820643e3fc3c2de7e45d07b7c10944e75694e
Christina-2024 <christina_johnson@live.co.uk>
2025-03-10 18:40:22 +0000

    Updated home page.

---
+++ destinations/destinations.js
@@ -0,0 +1,11 @@
+function fetchJSONData() {
+    fetch('./destinations.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();  
+        })
+        .then(data => console.log(data))  
+        .catch(error => console.error('Failed to fetch data:', error)); 
+}
